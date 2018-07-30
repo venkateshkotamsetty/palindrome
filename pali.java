a6dde0ca64b635433aacbbf85512f9f68a243fc9
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-07-30 15:51:13 +0530

    Create pali.java

---
+++ pali.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		  String original, reverse = ""; // Objects of String class  
+      Scanner in = new Scanner(System.in);   
+      original = in.nextLine();   
+      int length = original.length();   
+      for ( int i = length - 1; i >= 0; i-- )  
+         reverse = reverse + original.charAt(i);  
+      if (original.equals(reverse))  
+         System.out.println("yes");  
+      else  
+         System.out.println("no");   
+   
+
+	}
+}
